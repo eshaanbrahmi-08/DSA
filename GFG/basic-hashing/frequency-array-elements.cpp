68b2cb72402e0bf4fc9c17d862075789357a0c41
eshaanbrahmi-08 <eshaanbrahmi@gmail.com>
2026-09-15 14:15:05 +0530

    Create frequency-array-elements.cpp

---
+++ GFG/basic-hashing/frequency-array-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+ 
+    vector<vector<int>> countFreq(vector<int>& arr) {
+        map<int,int> mpp;
+        vector <vector<int>> vec;
+    
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            mpp[arr[i]]++;
+        }
+        
+        for(auto it : mpp){
+            vec.push_back({it.first,it.second});
+        }
+        
+        return vec;
+        
+        
+        
+        
+    }
+};
